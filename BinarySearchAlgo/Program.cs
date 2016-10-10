e2af1e43bae59622b5b6f3270e4b6991a18e2269
Eidiv <Eidiv@DESKTOP-8SKESLJ>
2016-10-10 22:38:02 +0200

    Add project files.

---
+++ BinarySearchAlgo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinarySearchAlgoSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 16, 17, 18 };
+            Console.WriteLine("The item index is : {0}" , BinarySearchAlgo<int>(arr, 5, 0, arr.GetUpperBound(0)));
+            Console.ReadLine();
+        }
+
+
+        static int BinarySearchAlgo<T>(T[] list, T item, int min, int max)
+        {
+            while (min <= max)
+            {
+                Comparer<T> comp = Comparer<T>.Default;
+                int indexComp = (min + max) / 2;
+                if (comp.Compare(item, list[indexComp]) == 0) return indexComp;
+                else if (comp.Compare(item, list[indexComp]) < 0) max = --indexComp;
+                else min = ++indexComp;
+            }
+            return 0;
+        }
+    }
+}
